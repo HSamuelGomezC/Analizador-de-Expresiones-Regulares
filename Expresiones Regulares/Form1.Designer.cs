0451621a15e55302aef72d384b76e51d5ab420b1
HSamuelGomezC <gomezcasillash@gmail.com>
2024-09-03 17:20:31 -0600

    Se subio el proyecto

---
+++ Expresiones Regulares/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace Expresiones_Regulares
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TextContraseña = new System.Windows.Forms.TextBox();
+            this.TextTelefono = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TextSS = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TextDireccion = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnContraseña = new System.Windows.Forms.Button();
+            this.btnTelefono = new System.Windows.Forms.Button();
+            this.btnSS = new System.Windows.Forms.Button();
+            this.btnDireccion = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.PeachPuff;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(241, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "EXPRESIONES REGULARES";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(130, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Contraseñas seguras \r\n";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // TextContraseña
+            // 
+            this.TextContraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextContraseña.Location = new System.Drawing.Point(448, 61);
+            this.TextContraseña.Name = "TextContraseña";
+            this.TextContraseña.Size = new System.Drawing.Size(210, 22);
+            this.TextContraseña.TabIndex = 2;
+            this.TextContraseña.TextChanged += new System.EventHandler(this.TextContraseña_TextChanged);
+            // 
+            // TextTelefono
+            // 
+            this.TextTelefono.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextTelefono.Location = new System.Drawing.Point(448, 124);
+            this.TextTelefono.Name = "TextTelefono";
+            this.TextTelefono.Size = new System.Drawing.Size(210, 22);
+            this.TextTelefono.TabIndex = 4;
+            this.TextTelefono.TextChanged += new System.EventHandler(this.TextTelefono_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(91, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(243, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Número de teléfono internacional";
+            // 
+            // TextSS
+            // 
+            this.TextSS.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextSS.Location = new System.Drawing.Point(448, 195);
+            this.TextSS.Name = "TextSS";
+            this.TextSS.Size = new System.Drawing.Size(210, 22);
+            this.TextSS.TabIndex = 6;
+            this.TextSS.TextChanged += new System.EventHandler(this.TextSS_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(120, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(184, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Número de seguro social";
+            // 
+            // TextDireccion
+            // 
+            this.TextDireccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextDireccion.Location = new System.Drawing.Point(448, 264);
+            this.TextDireccion.Name = "TextDireccion";
+            this.TextDireccion.Size = new System.Drawing.Size(210, 22);
+            this.TextDireccion.TabIndex = 8;
+            this.TextDireccion.TextChanged += new System.EventHandler(this.TextDireccion_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(155, 264);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(115, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Dirección física";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 81);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(366, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "(mínimo 8 caracteres, al menos una mayúscula, una minúscula y un número)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(147, 144);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(130, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "(con o sin código de país)";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(149, 215);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(126, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "( formato XXX-XX-XXXX )";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(72, 284);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(280, 13);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "(Ejemplo de patrón para direcciones: 123 Main St, Apt 4B)";
+            // 
+            // btnContraseña
+            // 
+            this.btnContraseña.Location = new System.Drawing.Point(583, 89);
+            this.btnContraseña.Name = "btnContraseña";
+            this.btnContraseña.Size = new System.Drawing.Size(75, 23);
+            this.btnContraseña.TabIndex = 13;
+            this.btnContraseña.Text = "Probar";
+            this.btnContraseña.UseVisualStyleBackColor = true;
+            this.btnContraseña.Click += new System.EventHandler(this.btnContraseña_Click);
+            // 
+            // btnTelefono
+            // 
+            this.btnTelefono.Location = new System.Drawing.Point(583, 152);
+            this.btnTelefono.Name = "btnTelefono";
+            this.btnTelefono.Size = new System.Drawing.Size(75, 23);
+            this.btnTelefono.TabIndex = 14;
+            this.btnTelefono.Text = "Probar";
+            this.btnTelefono.UseVisualStyleBackColor = true;
+            this.btnTelefono.Click += new System.EventHandler(this.btnTelefono_Click);
+            // 
+            // btnSS
+            // 
+            this.btnSS.Location = new System.Drawing.Point(583, 223);
+            this.btnSS.Name = "btnSS";
+            this.btnSS.Size = new System.Drawing.Size(75, 23);
+            this.btnSS.TabIndex = 15;
+            this.btnSS.Text = "Probar";
+            this.btnSS.UseVisualStyleBackColor = true;
+            this.btnSS.Click += new System.EventHandler(this.btnSS_Click);
+            // 
+            // btnDireccion
+            // 
+            this.btnDireccion.Location = new System.Drawing.Point(583, 292);
+            this.btnDireccion.Name = "btnDireccion";
+            this.btnDireccion.Size = new System.Drawing.Size(75, 23);
+            this.btnDireccion.TabIndex = 16;
+            this.btnDireccion.Text = "Probar";
+            this.btnDireccion.UseVisualStyleBackColor = true;
+            this.btnDireccion.Click += new System.EventHandler(this.btnDireccion_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cornsilk;
+            this.ClientSize = new System.Drawing.Size(695, 339);
+            this.Controls.Add(this.btnDireccion);
+            this.Controls.Add(this.btnSS);
+            this.Controls.Add(this.btnTelefono);
+            this.Controls.Add(this.btnContraseña);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.TextDireccion);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TextSS);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TextTelefono);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TextContraseña);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "ER";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TextContraseña;
+        private System.Windows.Forms.TextBox TextTelefono;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TextSS;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TextDireccion;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnContraseña;
+        private System.Windows.Forms.Button btnTelefono;
+        private System.Windows.Forms.Button btnSS;
+        private System.Windows.Forms.Button btnDireccion;
+    }
+}
+
---
+++ Expresiones Regulares/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Expresiones_Regulares
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextContraseña_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextTelefono_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextSS_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextDireccion_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnContraseña_Click(object sender, EventArgs e)
+        {
+            string input = TextContraseña.Text;
+            string patron = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$";
+
+            if (Regex.IsMatch(input, patron))
+            {
+                MessageBox.Show("Cadena SI aceptada");
+            }
+            else
+            {
+                MessageBox.Show("Cadena NO aceptada");
+            }
+        }
+
+        private void btnTelefono_Click(object sender, EventArgs e)
+        {
+            string input = TextTelefono.Text;
+            string patron = @"^\+?\d{1,3}[-.\s]?\(?\d{1,4}\)?[-.\s]?\d{1,10}$";
+
+            if (Regex.IsMatch(input, patron))
+            {
+                MessageBox.Show("Cadena SI aceptada");
+            }
+            else
+            {
+                MessageBox.Show("Cadena NO aceptada");
+            }
+        }
+
+        private void btnSS_Click(object sender, EventArgs e)
+        {
+            string input = TextSS.Text;
+            string patron = @"^\d{3}-\d{2}-\d{4}$";
+
+            if (Regex.IsMatch(input, patron))
+            {
+                MessageBox.Show("Cadena SI aceptada");
+            }
+            else
+            {
+                MessageBox.Show("Cadena NO aceptada");
+            }
+        }
+
+        private void btnDireccion_Click(object sender, EventArgs e)
+        {
+            string input = TextDireccion.Text;
+            string patron = @"^\d+\s[A-Za-z]+\s(St|Rd|Ave|Blvd)\,\s(Ste|Apt)\s\d+[A-Za-z]*$";
+
+            if (Regex.IsMatch(input, patron))
+            {
+                MessageBox.Show("Cadena SI aceptada");
+            }
+            else
+            {
+                MessageBox.Show("Cadena NO aceptada");
+            }
+        }
+    }
+}
